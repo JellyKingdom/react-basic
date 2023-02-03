048d97325697fb211e4e4de16f1186ca0800e4cf
JellyKingdom <jyhan1993@gmail.com>
2023-02-03 19:31:15 +0900

    feat: add <>

--- week-1/src/App.js
+++ week-1/src/App.js
@@ -1,15 +1,17 @@
-import 'App.css';
+import "App.css";
 
 //자식 컴포넌트
-function Child(){
-  return <div>나는 자식입니다.</div>
+function Child() {
+    return <div>나는 자식입니다.</div>;
 }
-
 
 //부모 컴포넌트
 function App() {
-
-  return <Child />;
+    return (
+        <>
+          <Child />
+        </>
+    );
 }
 
 export default App;
