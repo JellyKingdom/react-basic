e21be216c532c38558c40927053adc697161e870
JellyKingdom <jyhan1993@gmail.com>
2023-02-03 13:44:00 +0900

    feat: add template literal

---
+++ Day1/lecture1_5_template literal.js
@@ -0,0 +1,13 @@
+console.log("문자열입니다.");
+console.log(`문자열입니다.`); //백틱
+
+const testValue = 3;
+console.log(`숫자 ${testValue} 입니다.`); //${} placeholder
+
+const testValue2 =  `
+
+    완전 여러줄~
+    자유롭게 쓸 수 있습니다~
+    야호!
+
+`;
