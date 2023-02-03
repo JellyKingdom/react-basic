7054afca97550b553221d9217dec571533fa9a81
JellyKingdom <jyhan1993@gmail.com>
2023-02-03 19:59:32 +0900

    feat: add inline style

--- week-1/src/App.js
+++ week-1/src/App.js
@@ -2,11 +2,11 @@ import React from "react";
 
 function App() {
 
-    const number = 1;
+    const number = 11;
     return (
         <>
-            <div>
-              <p>안녕하세요 리액트입니다.</p>
+            <div className="test-class">
+              <p style={{color:'red'}}>안녕하세요 리액트입니다.</p>
               {/* 주석을 사용하는 방법입니다. */}
               {/* 삼항연산자를 사용해볼게요! */}
               <p>{number > 10 ? number + '은 10보다 크다' : number + '은 10보다 작다.'}</p>
