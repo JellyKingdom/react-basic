b0918788c8d4d772de33efef0ad56e6559d932d3
JellyKingdom <jyhan1993@gmail.com>
2023-02-03 19:56:14 +0900

    feat: annotation and conditional operator

--- week-1/src/App.js
+++ week-1/src/App.js
@@ -2,12 +2,14 @@ import React from "react";
 
 function App() {
 
-    const a = 3;
-    const b =10;
+    const number = 1;
     return (
         <>
             <div>
-                안녕하세요 {a + b}입니다.
+              <p>안녕하세요 리액트입니다.</p>
+              {/* 주석을 사용하는 방법입니다. */}
+              {/* 삼항연산자를 사용해볼게요! */}
+              <p>{number > 10 ? number + '은 10보다 크다' : number + '은 10보다 작다.'}</p>
             </div>
         </>
     );
