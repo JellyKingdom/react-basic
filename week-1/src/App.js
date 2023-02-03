c7c2fcf38e9447ba95faf4a06b76a1d17dbf6f41
JellyKingdom <jyhan1993@gmail.com>
2023-02-03 19:29:52 +0900

    feat: basic component

--- week-1/src/App.js
+++ week-1/src/App.js
@@ -1,24 +1,15 @@
-import logo from './logo.svg';
-// import './App.css';
 import 'App.css';
 
+//자식 컴포넌트
+function Child(){
+  return <div>나는 자식입니다.</div>
+}
+
+
+//부모 컴포넌트
 function App() {
-  //여기가 자바스크립트를 쓸 수 있는 영역입니다.
-  const a = 1;
-  function test(){
-    console.log("test 입니다!");
-  }
 
-
-  return (
-    //리턴문 안쪽에서는 JSX문법을 쓸 수 있다.
-    // JSX(Javascript + XML - HTML): 쟉스~
-    // {}
-    <div className="App">
-      <p>Hello react</p>
-      <button onClick={test}> 클릭해보세요 </button>
-    </div>
-  );
+  return <Child />;
 }
 
 export default App;
