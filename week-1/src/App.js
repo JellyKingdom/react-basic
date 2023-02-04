8fe654b1f2da4cdd1263d15c6d68f54e5841570c
JellyKingdom <jyhan1993@gmail.com>
2023-02-04 23:25:25 +0900

    feat: practice login page with state

--- week-1/src/App.js
+++ week-1/src/App.js
@@ -3,7 +3,8 @@ import Layout from "Layout";
 
 function App() {
 
-    const[fruit, setFruit] =useState("");
+    const [id,setId] = useState("");
+    const [pw, setPw] = useState("");
 
     return (
       <>
@@ -11,12 +12,18 @@ function App() {
         <div>STATE에 대한 이야기입니다.</div>
       </Layout>
       <div>
-        과일 : <input value={fruit} onChange={(e) => {
-          // console.log(e.target.value);
-          setFruit(e.target.value);
+        아이디 : <input type="text" value={id} onInput={(e) => {
+          setId(e.target.value);
+          }}/>
+        <br/>
+        비밀번호 : <input type="password" value={pw} onChange={(e) => {
+          setPw(e.target.value);
         }}/>
       </div>
-      {fruit}
+
+      <button onClick={()=>{
+        alert(`입력하신 아이디는 ${id}, 비밀번호는 ${pw}입니다.`);
+      }}>로그인</button>
       </>
     );
 }
