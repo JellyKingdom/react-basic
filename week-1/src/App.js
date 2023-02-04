698d37b47007f6e8d2d6080fbde20daa77e57f2c
JellyKingdom <jyhan1993@gmail.com>
2023-02-04 15:34:05 +0900

    feat: props1

--- week-1/src/App.js
+++ week-1/src/App.js
@@ -1,20 +1,24 @@
 import React from "react";
 
+function Child(){
+  return <div>나는 자식이에요!</div>;
+}
+
+//부모 -> 자식 정보를 전달했다!
+function Mother(){
+  const name = '엄마이름!';
+  return <Child motherName={name}/>;
+}
+
+function GrandFather(){
+  return <Mother />;
+}
+
 function App() {
 
-    const number = 11;
-
-    const pTagStyle = {
-      color: 'blue',
-    }
     return (
         <>
-            <div className="test-class">
-              <p style={{color:'red'}}>안녕하세요 리액트입니다.</p>
-              {/* 주석을 사용하는 방법입니다. */}
-              {/* 삼항연산자를 사용해볼게요! */}
-              <p style={pTagStyle}>{number > 10 ? number + '은 10보다 크다' : number + '은 10보다 작다.'}</p>
-            </div>
+          <GrandFather/>
         </>
     );
 }
