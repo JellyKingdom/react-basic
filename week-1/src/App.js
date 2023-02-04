815ce1d3cb75c61a6fdbe9d779fe17096011f8f7
JellyKingdom <jyhan1993@gmail.com>
2023-02-04 15:36:56 +0900

    feat: props2

--- week-1/src/App.js
+++ week-1/src/App.js
@@ -1,12 +1,14 @@
 import React from "react";
 
-function Child(){
-  return <div>나는 자식이에요!</div>;
+//props를 통해 부모 -> 자식 데이터가 전달된다.
+function Child(props){
+  console.log("props",props.motherName);
+  return <div>나는 {props.motherName} 을 아는 자식이에요!</div>;
 }
 
 //부모 -> 자식 정보를 전달했다!
 function Mother(){
-  const name = '엄마이름!';
+  const name = '엄마이름';
   return <Child motherName={name}/>;
 }
 
