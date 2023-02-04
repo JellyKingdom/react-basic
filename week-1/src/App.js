e6ca695929263f09500853fcfaf8c36cd20e346e
JellyKingdom <jyhan1993@gmail.com>
2023-02-04 15:40:20 +0900

    feat: props practice

--- week-1/src/App.js
+++ week-1/src/App.js
@@ -3,17 +3,19 @@ import React from "react";
 //props를 통해 부모 -> 자식 데이터가 전달된다.
 function Child(props){
   console.log("props",props.motherName);
-  return <div>나는 {props.motherName} 을 아는 자식이에요!</div>;
+  return <div>나는 {props.grandFatherName}과 {props.motherName} 을 아는 자식이에요!</div>;
 }
 
 //부모 -> 자식 정보를 전달했다!
-function Mother(){
+function Mother(props){
+  const grandpa = props.grandFatherName;
   const name = '엄마이름';
-  return <Child motherName={name}/>;
+  return <Child grandFatherName={grandpa} motherName={name}/>;
 }
 
 function GrandFather(){
-  return <Mother />;
+  const name ='할아버지 이름'
+  return <Mother grandFatherName={name} />;
 }
 
 function App() {
