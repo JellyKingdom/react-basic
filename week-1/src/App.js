40b71250a4f420fb9d5a7818f31ef0a7f01fc888
JellyKingdom <jyhan1993@gmail.com>
2023-02-04 16:19:20 +0900

    feat: destructing

--- week-1/src/App.js
+++ week-1/src/App.js
@@ -3,6 +3,19 @@ import Layout from "Layout";
 
 function App() {
     
+  //구조분해 할당!
+  const testObj = {
+    name : 'jiyun',
+    age :32,
+    company: 'Baek-su!'
+  }
+
+  const {name, age, company} = testObj;
+
+  console.log('name', name);
+  console.log('age',age);
+  console.log('company',company);
+
     return (
       <Layout>
         <div>App 컴포넌트에서 보낸 값 입니다.</div>
