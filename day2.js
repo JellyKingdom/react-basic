bd1278dcede04eab2700dd3c87a97f339d119e8c
JellyKingdom <jyhan1993@gmail.com>
2023-02-06 16:22:57 +0900

    feat: 원시데이터가 아닌데이터

--- day2.js
+++ day2.js
@@ -23,3 +23,11 @@ console.log(obj1 === obj2); //false
 //데이터를 수정하는 경우
 number = 2;
 console.log(number);    //메모리주소가 변경, 2 나온다.
+
+
+obj1.name = 'park'; //객체는 불변성이 없다!
+console.log(obj1.name); //park
+
+
+//원시데이터 -> 메모리에 저장 된 값 자체를 바꾸는게 아니라 저장공간을 바꾸는것
+//원시데이터가 아닌 데이터 -> 기존에 저장되어있던 공간을 그냥 바꿔버리는 것. 불변성 없음!
