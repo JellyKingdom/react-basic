c120922979156a34cf2665af5fc76eb03797d4f1
JellyKingdom <jyhan1993@gmail.com>
2023-02-06 16:15:36 +0900

    feat: 원시데이터 비교

--- day2.js
+++ day2.js
@@ -1,1 +1,19 @@
 // 불변성: 메모리에 있는 값을 변경할 수 없는 것.
+
+let number = 1;
+let secondNumber = 1;
+
+// 원시데이터 : 숫자, 문자, 불리언..
+console.log('number와 secondNumber가 일치하나요?');
+console.log(number === secondNumber);   //true
+
+// 원시데이터가 아닌 것들 : 배열, 객체, 함수...
+let obj1 = {
+    name: 'kim',
+}
+
+let obj2 = {
+    name: 'kim',
+}
+
+console.log(obj1 === obj2); //false
