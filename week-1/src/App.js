fb3d6edb43b54810845209d88aa59ba83f139e2a
JellyKingdom <jyhan1993@gmail.com>
2023-02-06 17:08:46 +0900

    feat: add counter state

--- week-1/src/App.js
+++ week-1/src/App.js
@@ -2,31 +2,14 @@ import React, { useState } from "react";
 import Layout from "Layout";
 
 function App() {
-  const [obj, setObj] = useState({
-    name: "jiyun",
-    age: 32,
-  });
+  const [counter, setCounter] = useState(0);
 
     return (
       <>
       <Layout>
-        <div>불변성 강의 중!</div>
+        <div>Counter App</div>
       </Layout>
-      <div>{obj.name}</div>
-      <button onClick={()=>{
-        obj.name = "jiyunHan"
-        console.log(obj);
-        setObj(obj);
-        // 콘솔은 찍히지만 렌더링되지는 않는다! 새로운 객체를 만들어줘야한다.
-        
-        const obj2 = {...obj};
-        console.log(obj2);
-        obj2.name = "jiyunHan";
-        setObj(obj2);
-
-      }}>
-      클릭!!</button>
-
+      
       </>
     );
 }
