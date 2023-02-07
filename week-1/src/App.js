7ecbe4207516fd7b96eecba977e8ff8c7cdaf391
JellyKingdom <jyhan1993@gmail.com>
2023-02-07 09:50:29 +0900

    feat: change variable to state

--- week-1/src/App.js
+++ week-1/src/App.js
@@ -3,12 +3,21 @@ import Layout from "Layout";
 import "App.css";
 
 function App() {
-    const users = [
-        { id: 1, age: 30, name: "송중기" },
-        { id: 2, age: 24, name: "송강" },
-        { id: 3, age: 21, name: "김유정" },
-        { id: 4, age: 29, name: "구교환" },
-    ];
+    // const users = [
+    //     { id: 1, age: 30, name: "송중기" },
+    //     { id: 2, age: 24, name: "송강" },
+    //     { id: 3, age: 21, name: "김유정" },
+    //     { id: 4, age: 29, name: "구교환" },
+    // ];
+
+    //단순 변수, 객체가 아니라 state로 써주자!
+
+    const [users, setUsers] = useState([
+      { id: 1, age: 30, name: "송중기" },
+      { id: 2, age: 24, name: "송강" },
+      { id: 3, age: 21, name: "김유정" },
+      { id: 4, age: 29, name: "구교환" },
+  ]);
 
     return (
         <>
