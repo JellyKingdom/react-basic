43450c0cdf941b31a4a5e254b6d38a12621e98fd
JellyKingdom <jyhan1993@gmail.com>
2023-02-07 09:37:01 +0900

    feat: practice 2

--- week-1/src/App.js
+++ week-1/src/App.js
@@ -1,32 +1,23 @@
 import React, { useState } from "react";
 import Layout from "Layout";
-import 'App.css';
+import "App.css";
 
 function App() {
-
-    const vegi = ['감자','고구마','오이','가지','옥수수'];
+    const users = [
+        { id: 1, age: 30, name: "송중기" },
+        { id: 2, age: 24, name: "송강" },
+        { id: 3, age: 21, name: "김유정" },
+        { id: 4, age: 29, name: "구교환" },
+    ];
 
     return (
-      <>
-      <Layout>
-        <div>Style project</div>
-        
-      </Layout> 
+        <>
+            <Layout>
+                <div>component practice</div>
+            </Layout>
 
-      <div className="container">
-        {
-          vegi.filter(function(item){
-            return item !== "오이";
-          })
-          .map(function(item){
-            return <div className="vegetable">{item}</div>
-          })
-        }
-      </div>
-      
-    
-
-      </>
+            
+        </>
     );
 }
 
