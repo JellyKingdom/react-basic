5b713064fed9e093956baef6742a0ba6014391de
JellyKingdom <jyhan1993@gmail.com>
2023-02-07 09:24:07 +0900

    feat: practice map and filter

--- day3.js
+++ day3.js
@@ -1,9 +1,13 @@
 const test =[ 1,2,3 ];
 
-console.log(test.map(function(i){
-    return i * 2;
+console.log(test.map(function(item){
+    return item * 2;
 }));    //2,4,6
 
 console.log(test);  //1,2,3
 
 //map 함수는 원래 데이터를 가공하여 출력하는 방법이다.
+
+console.log(test.filter(function(item){
+    return item < 3 ;
+}));    //1,2
